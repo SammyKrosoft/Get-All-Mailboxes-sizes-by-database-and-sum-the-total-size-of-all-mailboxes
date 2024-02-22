660626d082e73a59ac095f37bbe159537990b953
Sam Drey <33433229+SammyKrosoft@users.noreply.github.com>
2024-02-22 17:39:17 -0500

    Update ForMike-GetMailboxSizesAndSumTotal.ps1

--- ForMike-GetMailboxSizesAndSumTotal.ps1
+++ ForMike-GetMailboxSizesAndSumTotal.ps1
@@ -22,8 +22,14 @@ ForEach ($Database in $Databases) {
     Write-Progress -Id 1 -Activity "Calculating Mailbox Sizes" -Status "Calculating Mailbox Sizes for Database: $($Database.Name)" -PercentComplete $percentCompleteDB
 
     # Get all mailboxes in the database
+    # NOTE: I am initializing an array and using += to get mailboxes to cover the cases where we have just one mailbox on the database.
+    # Otherwise if you are sure you have no cases where you just have 1 mailbox in the database 
+    # you can just use $Mailboxes = Get-Mailbox -Database $Database.Identity  | Select Identity | Get-MailboxStatistics | Select DisplayName, PrimarySMTPADdress, TotalItemSize, TotalDeletedItemSize
+    # NOTE2: for the cases where you have just 1 mailbox, you can also treat that case in a separate IF statement.
+    # NOTE3: this is because if you have just 1 mailbox, the $Mailboxes variable is not an array by default. So we "force" it to be an array at the first place, and it will be a 1 item array in case
+    # we have just 1 mailbox returned by the Get-Mailbox statement!
     $MAilboxes = @()
-    $Mailboxes += Get-Mailbox -Database $Database.Identity -Filter {Name -notlike "*DiscoverySearchMailbox*"} -ResultSize Unlimited | Get-MailboxStatistics | Select DisplayName, PrimarySMTPADdress, TotalItemSize, TotalDeletedItemSize
+    $Mailboxes += Get-Mailbox -Database $Database.Identity -Filter {Name -notlike "*DiscoverySearchMailbox*"} | Select Identity | % {Get-MailboxStatistics -Identity $_.Identity | Select DisplayName, PrimarySMTPADdress, TotalItemSize, TotalDeletedItemSize}
     Write-Host "Nb Mailboxes: $($Mailboxes.count)" -ForegroundColor Red
     If ($MAilboxes.Count -gt 0){
         # Loop through each mailbox
@@ -64,7 +70,7 @@ $TotalSizeOfMAilboxesKB = ($MailboxSizeCollection | Measure-Object -Property "Mb
 $TotalSizeOfMailboxesMB = ($MailboxSizeCollection | Measure-Object -Property "MbxSize(In MB)" -Sum).Sum
 $TotalSizeOfMailboxesGB = ($MailboxSizeCollection | Measure-Object -Property "MbxSize(In GB)" -Sum).Sum
 
-Write-Host "`n`nTotal Number of Mailboxes: $NumberOfMailboxes`n`n" -ForegroundColor Yellow -BackgroundColor DarkBlue
+Write-Host "Total Number of Mailboxes: $NumberOfMailboxes`n`n" -ForegroundColor Yellow -BackgroundColor DarkBlue
 
 Write-Host "Total Size of Mailboxes in KB: $TotalSizeOfMailboxesKB KB" -ForegroundColor Green -BackgroundColor Blue
 Write-Host "Total Size of Mailboxes in MB: $TotalSizeOfMailboxesMB MB" -ForegroundColor Yellow -BackgroundColor Blue
